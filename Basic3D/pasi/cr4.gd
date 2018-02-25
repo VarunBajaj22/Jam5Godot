922c464b62bfe4c7f934bf6c502f89f2886d83fc
cr3186 <cr3186@IGM2025-32>
2018-02-25 17:00:51 -0500

    timebar

--- Basic3D/pasi/cr4.gd
+++ Basic3D/pasi/cr4.gd
@@ -19,8 +19,10 @@ func _ready():
 func _on_Area_body_entered(body):
     if body.name=="Player":
         if a<3:
+	        
 	        $Area/MeshInstance.hide()
 	        $AudioStreamPlayer.play()
 	        $Area.hide();
+	        get_node("../TextureProgress/timebar").value+=1
 	        $Area/CollisionShape.hide()
 	        a=5;
