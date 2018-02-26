382651f853460f27a054cf4da4b267dd58267f38
sxl9302 <sxl9302@g.rit.edu>
2018-02-25 20:57:43 -0500

    Working on trap boxes

--- Basic3D/pasi/cr4.gd
+++ Basic3D/pasi/cr4.gd
@@ -17,12 +17,12 @@ func _ready():
 
 
 func _on_Area_body_entered(body):
-    if body.name=="Player":
-        if a<3:
-	        
-	        $Area/MeshInstance.hide()
-	        $AudioStreamPlayer.play()
-	        $Area.hide();
-	        get_node("../TextureProgress/timebar").value+=1
-	        $Area/CollisionShape.hide()
-	        a=5;
+	if body.name=="Player":
+		if a<3:
+			$Area/MeshInstance.hide()
+			$AudioStreamPlayer.play()
+			$Area.hide();
+			print(get_tree().get_root().get_node("TextureProgress"))
+			#get_node("../TextureProgress/timebar").value+=1
+			$Area/CollisionShape.hide()
+			a=5;
